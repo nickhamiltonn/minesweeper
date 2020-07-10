2f2177147c4d830d1f93f2938f3e0893cf30f472
nickhamiltonn <nick.hamilton@shaw.ca>
2020-07-09 23:15:09 -0700

    First Commit for minesweeper.js, simple hard coded implementation, as found in CodeCademy lesson

---
+++ src/minesweeper.js
@@ -0,0 +1,16 @@
+// Initializing hard-coded board option
+const blankLine = '  |   |  ';
+const guessLine = '1 |   |  ';
+const bombLine = '  | B |  ';
+
+// Displaying empty board
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+// Displaying a possible board configuration
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
