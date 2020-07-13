e5b5ed48a8c5a331079a858b49e5394d1434aee8
nickhamiltonn <nick.hamilton@shaw.ca>
2020-07-13 15:53:52 -0700

    Another hard coded solution, with a print gameboard function. Part 2 of the CodeCademy minesweeper project

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,16 +1,17 @@
-// Initializing hard-coded board option
-const blankLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
+const printBoard = board => {
+  console.log('Current Board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
 
-// Displaying empty board
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+}
+let board = [[' ', ' ', ' '],
+             [' ', ' ', ' '],
+             [' ', ' ', ' ']];
 
-// Displaying a possible board configuration
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+printBoard(board);
+
+board[0][1] = '1';
+board[2][2] = 'B';
+
+printBoard(board);
